1741d834531bbd1c9deda59ff3ab7c89d90446bd
JoaoVitorSil <joaovitorsil13@gmail.com>
2022-11-16 11:40:35 -0300

    Criando um service e utilizando chamadas HTTP

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,6 +1,8 @@
+import { CursosService } from './../services/cursos.service';
 import { Component, OnInit } from '@angular/core';
 
 import { Curso } from './../model/curso';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
@@ -8,15 +10,18 @@ import { Curso } from './../model/curso';
   styleUrls: ['./cursos.component.scss']
 })
 export class CursosComponent implements OnInit {
-  cursos: Curso[] = [
-    {_id: '1', name: 'Angular', category:'Front-end'}
-  ];
+  cursos: Observable<Curso[]>;
   displayedColumns = ['name','category'];
 
-  constructor() {
+  //cursosService: CursosService;
+
+  constructor(private cursosService: CursosService) {
+    //this.cursosService = new CursosService();
+    this.cursos = this.cursosService.list();
   }
 
   ngOnInit(): void {
+
   }
 
 }
---
+++ src/app/cursos/services/cursos.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs/operators';
+
+import { Curso } from './../model/curso';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  private readonly API =  '/assets/cursos.json';
+
+  constructor(private httpClient: HttpClient) { }
+
+  list(){
+    return this.httpClient.get<Curso[]>(this.API).
+    pipe(
+      first(),
+      tap(cursos => console.log(cursos))
+    );
+  }
+}
